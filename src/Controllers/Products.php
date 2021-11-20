4663eb0315d54f626b06022c2d897b5e9e8e24e4
WalterIran <amadorwalter3418@gmail.com>
2021-11-20 16:57:02 -0600

    share btn functionality ready on each product of search screen

--- src/Controllers/Products.php
+++ src/Controllers/Products.php
@@ -94,9 +94,10 @@ class Products extends PublicController
         );
 
         $viewData["products"] = $products;//json_encode($products);
+        if(isset($_GET["search"])){
+            $viewData["search"] = $_GET["search"];
+        }
 
-        //$viewData["products"] = [1,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10];
-        //dd($viewData);S
         \Views\Renderer::render("products/search",$viewData);
     }
 }
